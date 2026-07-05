4587a8438d9f9d07006664ea5da1a9bd27c2b373
Zaldy Aditya Pashafa <zapashafa2@gmail.com>
2026-07-05 14:39:04 +0700

    first

--- portal.js
+++ portal.js
@@ -1,35 +1,43 @@
 if(window.location.pathname.includes("index.html")){
-
-    alert("Selamat datang di halaman utama");
-
+function bukaPopup(){
+document.getElementById("popup").style.display="flex";
 }
-
-
+function tutupPopup(){
+    document.getElementById("popup").style.display="none";
+}
+window.onload=function(){
+    bukaPopup();
+}
 function porto(){
-
-    let jawab = confirm("Apakah Anda ingin membuka portofolio?");
-
-    if(jawab){
-
-        window.location.href = "portofolio.html";
-
-    }
+    document.getElementById("pop").style.display="flex";
+}
+function lanjut(){
+    window.location.href="portofolio.html";
+}
+function batal(){
+    document.getElementById("pop").style.display="none";
+}
 
 }
+
 if(window.location.pathname.includes("portofolio.html")){
-
-    alert("Selamat datang di CV Zaldy Aditya Pashafa!");
-
+function bukaPopup(){
+document.getElementById("popup").style.display="flex";
 }
-
+function tutupPopup(){
+    document.getElementById("popup").style.display="none";
+}
+window.onload=function(){
+    bukaPopup();
+}
 function kembali(){
-
-    let jawab = confirm("Apakah Anda yakin ingin kembali ke halaman utama?");
-
-    if(jawab){
-
-        window.location.href = "index.html";
-
-    }
+    document.getElementById("pop").style.display="flex";
+}
+function lanjut(){
+    window.location.href="index.html";
+}
+function batal(){
+    document.getElementById("pop").style.display="none";
+}
 
 }
