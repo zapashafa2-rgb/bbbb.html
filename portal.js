aa60d8a54695e054d0b7d529351147542feda8f0
Zaldy Aditya Pashafa <zapashafa2@gmail.com>
2026-07-05 19:28:32 +0700

    first

--- portal.js
+++ portal.js
@@ -1,4 +1,3 @@
-if(window.location.pathname.includes("index.html")){
 function bukaPopup(){
 document.getElementById("popup").style.display="flex";
 }
@@ -18,26 +17,23 @@ function batal(){
     document.getElementById("pop").style.display="none";
 }
 
-}
 
-if(window.location.pathname.includes("portofolio.html")){
 function bukaPopup(){
-document.getElementById("popup").style.display="flex";
+document.getElementById("melayang").style.display="flex";
 }
 function tutupPopup(){
-    document.getElementById("popup").style.display="none";
+    document.getElementById("melayang").style.display="none";
 }
 window.onload=function(){
     bukaPopup();
 }
 function kembali(){
-    document.getElementById("pop").style.display="flex";
+    document.getElementById("jendela").style.display="flex";
 }
 function lanjut(){
     window.location.href="index.html";
 }
 function batal(){
-    document.getElementById("pop").style.display="none";
+    document.getElementById("jendela").style.display="none";
 }
 
-}
